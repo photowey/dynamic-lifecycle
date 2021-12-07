5dfb4e07c7ba0c178d8d440dc9cae6093a9f121d
photowey <photowey@gmail.com>
2021-12-07 23:46:40 +0800

    chore: add commands.

---
+++ dynamic-lifecycle-telnet/dynamic-lifecycle-telnet-remoting/src/main/java/com/photowey/dynamic/lifecycle/telnet/remoting/cmd/command/UninstallCommand.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright © 2021 photowey (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.dynamic.lifecycle.telnet.remoting.cmd.command;
+
+/**
+ * {@code UninstallCommand}
+ *
+ * @author photowey
+ * @date 2021/12/07
+ * @since 1.0.0
+ */
+public class UninstallCommand extends AbstractCommandAdaptor {
+
+    public UninstallCommand(String command) {
+        this.command = command;
+    }
+
+    @Override
+    public String name() {
+        return "uninstall";
+    }
+
+    @Override
+    public boolean validate() {
+        return this.command.contains(" --uninstall ") || this.command.contains(" -ui ");
+    }
+
+    @Override
+    protected String doExecute(DynamicLifecycleCommandLine commandLine) throws Exception {
+        return null;
+    }
+}
